7c6364780119391563471400a3d0715c69842a38
tjtjtjtj <testmail>
2017-11-02 08:07:56 +0000

    update

---
+++ sh/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+HASH=$(git rev-parse --verify HEAD)
+BUILDDATE=$(date '+%Y/%m/%d %H:%M:%S %Z')
+GOVERSION=$(go version)
+
+go build -ldflags "-s -w -X main.hash=${HASH} -X \"main.builddate=${BUILDDATE}\" -X \"main.goversion=${GOVERSION}\""
